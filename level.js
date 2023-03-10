69cabf23346712356974fe693f002281d4cb23d1
KristinaFi <75157579+KristinaFi@users.noreply.github.com>
2023-03-10 15:14:36 +0900

    Update level.js

--- level.js
+++ level.js
@@ -33,29 +33,29 @@ function SvoystoDisplay(id){
 function HideCalls(diff){
 
     if (diff == 'easy'){
-        if (SvoystoDisplay('Cat1Call1') == 'block'){
+        if (SvoystoDisplay('Call1') == 'block'){
             viewCall1()
         }
-        if (SvoystoDisplay('Cat1Call2') == 'block'){
+        if (SvoystoDisplay('Call2') == 'block'){
             viewCall2()
         }
-        if (SvoystoDisplay('Cat1Call3') == 'block'){
+        if (SvoystoDisplay('Call3') == 'block'){
             viewCall3()
         }
     }
     if (diff == 'middle'){
-        if (SvoystoDisplay('Cat1Call4') == 'block'){
+        if (SvoystoDisplay('Call4') == 'block'){
             viewCall4()
         }
-        if (SvoystoDisplay('Cat1Call5') == 'block'){
+        if (SvoystoDisplay('Call5') == 'block'){
             viewCall5()
         }
     }
     if (diff == 'hard'){
-        if (SvoystoDisplay('Cat1Call6') == 'block'){
+        if (SvoystoDisplay('Call6') == 'block'){
             viewCall6()
         }
-        if (SvoystoDisplay('Cat1Call7') == 'block'){
+        if (SvoystoDisplay('Call7') == 'block'){
             viewCall7()
         }
     }
@@ -100,41 +100,41 @@ function viewDivHard(){
 
 var Call1 = 0
 function viewCall1() {
-    nadoProverit('Cat1Call1');
+    nadoProverit('Call1');
 }
 
 var Call2 = 0
 function viewCall2() {
-    nadoProverit('Cat1Call2');
+    nadoProverit('Call2');
 }
 
 var Call3 = 0
 function viewCall3() {
-    nadoProverit('Cat1Call3');
+    nadoProverit('Call3');
 
 }
 
 var Call4 = 0
 function viewCall4() {
-    nadoProverit('Cat1Call4');
+    nadoProverit('Call4');
 }
 
 var Call5 = 0
 function viewCall5() {
-    nadoProverit('Cat1Call5');
+    nadoProverit('Call5');
 }
 
 var Call6 = 0
 function viewCall6() {
-     nadoProverit('Cat1Call6');
+     nadoProverit('Call6');
 }
 
 var Call7 = 0
 function viewCall7() {
-    nadoProverit('Cat1Call7');
+    nadoProverit('Call7');
 }
 
 var Call8 = 0
 function viewCall8() {
-    nadoProverit('Cat1Call8');
+    nadoProverit('Call8');
 }
